aa87daff3fd6eb84c15549d50cd7dbcef27fc4e7
ardiaz8 <angel.rafadg@gmail.com>
2025-10-17 18:22:22 -0600

    Added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,17 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+    
+    // 2) Upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`) // telling the AWS CLI
+// which local folders should be synchronized
+// with which remote S3 bucket.
+
     core.notice('Hello from my custom JavaScript Action!')
 }
 
